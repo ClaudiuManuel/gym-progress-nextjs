7528140e0580eedd909654f8632fbf646e9bdfb7
ClaudiuManuel <calian.claudiu@yahoo.com>
2025-01-30 21:02:00 +0200

    prettier config changes

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,6 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 export default {
   plugins: ["prettier-plugin-tailwindcss"],
+  printWidth: 120,
+  singleQuote: true,
 };
